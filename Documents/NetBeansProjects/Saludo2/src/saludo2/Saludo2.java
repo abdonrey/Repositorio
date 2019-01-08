97c3f4ff5b7b74f968c1f4ede9f9b17c685d1557
Abdon <abdonsanchezuno@gmail.com>
2019-01-08 01:04:56 -0300

    Ahora aporte "Hola Abdòn" archivo Saludo2.java

--- Documents/NetBeansProjects/Saludo2/src/saludo2/Saludo2.java
+++ Documents/NetBeansProjects/Saludo2/src/saludo2/Saludo2.java
@@ -17,6 +17,8 @@ public class Saludo2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo!");
+        
+        System.out.println("Hola Abdòn");
     }
     
 }
